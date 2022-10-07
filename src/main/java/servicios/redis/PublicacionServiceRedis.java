269d1715d8afcbccc78ecc570b45c44d36e4b549
Emilio Martin <emiunrn@gmail.com>
2022-10-07 20:48:01 -0300

    try with resourses en servicio publicaciones

--- src/main/java/servicios/redis/PublicacionServiceRedis.java
+++ src/main/java/servicios/redis/PublicacionServiceRedis.java
@@ -3,7 +3,6 @@ package servicios.redis;
 import api.PublicacionService;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.search.Document;
 import java.util.List;
@@ -18,65 +17,79 @@ public class PublicacionServiceRedis implements PublicacionService {
 
     @Override
     public String ultimasPublicaciones() {
-        JedisPooled jedis = new JedisPooled(this.url);
-        List<String> pubsPorFecha = jedis.zrange("pubsPorFecha", 0, 3);
-        List<JSONArray> jsonArray = jedis.jsonMGet(pubsPorFecha.toArray(new String[pubsPorFecha.size()]));
-        JSONArray jsonArrayPlain = new JSONArray();
-        for (JSONArray json : jsonArray) {
-            jsonArrayPlain.put(json.getJSONObject(0));
+        try (JedisPooled jedis = new JedisPooled(this.url)) {
+            List<String> pubsPorFecha = jedis.zrange("pubsPorFecha", 0, 3);
+            List<JSONArray> jsonArray = jedis.jsonMGet(pubsPorFecha.toArray(new String[pubsPorFecha.size()]));
+            JSONArray jsonArrayPlain = new JSONArray();
+            for (JSONArray json : jsonArray) {
+                jsonArrayPlain.put(json.getJSONObject(0));
+            }
+            return jsonArrayPlain.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
-        return jsonArrayPlain.toString();
     }
 
     @Override
     public String publicacion(String publicacionId) {
-        // try with resourses add
-        JedisPooled jedis = new JedisPooled(this.url);
-        Object jsonObject = jedis.jsonGet("post:" + publicacionId);
-        JSONArray jsonArray = new JSONArray();
-        jsonArray.put(jsonObject);
-        return jsonArray.toString();
+        try (JedisPooled jedis = new JedisPooled(this.url)) {
+            Object jsonObject = jedis.jsonGet("post:" + publicacionId);
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.put(jsonObject);
+            return jsonArray.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public String publicaciones(String nombre) {
-        JedisPooled jedis = new JedisPooled(this.url);
-        List<Document> resultado = jedis.ftSearch("postIdx", "@autor:" + nombre).getDocuments();
-        JSONArray jsonArray = new JSONArray();
-        for (Document document : resultado) {
-            document.getProperties().forEach(p -> {
-                jsonArray.put(p.getValue());
-            });
+        try (JedisPooled jedis = new JedisPooled(this.url)) {
+            List<Document> resultado = jedis.ftSearch("postIdx", "@autor:" + nombre).getDocuments();
+            JSONArray jsonArray = new JSONArray();
+            for (Document document : resultado) {
+                document.getProperties().forEach(p -> {
+                    jsonArray.put(p.getValue());
+                });
+            }
+            return jsonArray.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
-        return jsonArray.toString();
     }
 
     @Override
     public String autoresPublicaciones() {
-        Jedis jedis = new Jedis(this.url);
-        Map<String, String> resultado = jedis.hgetAll("autor:pubs");
-        JSONArray resultadoJSON = new JSONArray();
-        resultado.entrySet().stream()
-                .map(e -> {
-                    JSONObject address = new JSONObject();
-                    address.put("_id", e.getKey());
-                    address.put("count", e.getValue());
-                    return address;
-                })
-                .forEach(resultadoJSON::put);
-        return resultadoJSON.toString();
+        try (JedisPooled jedis = new JedisPooled(this.url)) {
+            Map<String, String> resultado = jedis.hgetAll("autor:pubs");
+            JSONArray resultadoJSON = new JSONArray();
+            resultado.entrySet().stream()
+                    .map(e -> {
+                        JSONObject address = new JSONObject();
+                        address.put("_id", e.getKey());
+                        address.put("count", e.getValue());
+                        return address;
+                    })
+                    .forEach(resultadoJSON::put);
+            return resultadoJSON.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
     public String publicacionesPorTexto(String texto) {
-        JedisPooled jedis = new JedisPooled(this.url);
-        List<Document> resultado = jedis.ftSearch("postIdx", "@texto:" + texto).getDocuments();
-        JSONArray jsonArray = new JSONArray();
-        for (Document document : resultado) {
-            document.getProperties().forEach(p -> {
-                jsonArray.put(p.getValue());
-            });
+        try (JedisPooled jedis = new JedisPooled(this.url)) {
+            List<Document> resultado = jedis.ftSearch("postIdx", "@texto:" + texto).getDocuments();
+            JSONArray jsonArray = new JSONArray();
+            for (Document document : resultado) {
+                document.getProperties().forEach(p -> {
+                    jsonArray.put(p.getValue());
+                });
+            }
+            return jsonArray.toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
         }
-        return jsonArray.toString();
     }
 }
